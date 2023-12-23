77e6311ae90d3c91e4208d17cb9eeca9228c25a4
jemprojects <jemprojectsgit@gmail.com>
2023-12-23 17:54:32 -0300

    part1 proyects

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,62 +1,92 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.'
+];
+
+const votes = Array(anecdotes.length).fill(0);
+
+function MostVotedAnecdote({ anecdote, highestVote }) {
+  if (highestVote === 0) {
+    return <p>No anecdote was voted yet.</p>;
+  }
+  return (
+    <>
+      <h3>&quot;{anecdote}&quot;</h3>
+      <DisplayVote voteCount={highestVote} />
+    </>
+  );
+}
+
+const DisplayVote = ({voteCount}) => (
+  <>
+    has {voteCount} votes
+  </>
+)
+// boton accionador
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
-const DisplayVote = (props) => (
-  <>
-    has {props.voteCount} votes
-  </>
-)
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+function App() {
+  const [selected, setSelected] = useState(0);
+  const [anecdotesVotes, setAnecdotesVotes] = useState(votes);
 
-  const [selected, setSelected] = useState(0)
-
-
-  const pointsMap = new Map();
-  for (var i = 0; i < anecdotes.length; i++) {
-    pointsMap.set(i, 0)
-  }
-  const [points, setPoints] = useState({pointsMap})
-
-  const getRandomAnecdoteIndex = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
+  function handleSelected() {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
   }
 
-  const handleVote = () => {
-    const copy = {...points}
-    copy.pointsMap.set(selected, copy.pointsMap.get(selected) + 1)
-    setPoints(copy)
+  function handleVote() {
+    const newAnecdotesVotes = [...anecdotesVotes];
+    newAnecdotesVotes[selected] += 1;
+    setAnecdotesVotes(newAnecdotesVotes);
+  }
+
+  function mostVoted() {
+    let index = 0;
+    let highestVote = 0;
+    for (let i = 0; i < anecdotesVotes.length; i++) {
+      if (anecdotesVotes[i] > highestVote) {
+        highestVote = anecdotesVotes[i];
+        index = i;
+      }
+    }
+
+    return { index, highestVote };
   }
 
   return (
-    <>
-      <div>
-        {anecdotes[selected]}
-      </div>
-      <div>
-        <DisplayVote voteCount={points.pointsMap.get(selected)} />
-      </div>
-      <div>
-        <Button text='vote' handleClick={handleVote} />
-        <Button text='next anecdote' handleClick={getRandomAnecdoteIndex} />
-      </div>
-    </>
-  )
+    <div>
+      <h2>Anecdote of the Day</h2>
+      <h3>&quot;{anecdotes[selected]}&quot;</h3>
+      <p>has {anecdotesVotes[selected]} votes</p>
+      <Button
+        handleClick={handleVote}
+        text='👍 Vote'
+      />
+        <Button
+        handleClick={handleSelected}
+        text='Next Anecdote'
+      />
+
+      <h2>Anecdote with most Votes</h2>
+      <MostVotedAnecdote
+        anecdote={anecdotes[mostVoted().index]}
+        highestVote={mostVoted().highestVote}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+// SALUDANDO Y CALCULANDO FECHA NAC
 const Hello = (props) => {
+  const hello = (who) => () => {
+    console.log('hello', who)
+  }
+// Desestructuración
+  const { name, age } = props
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+      <button onClick={hello('world')}>Console world</button>
+      <button onClick={hello('react')}>Console react</button>
     </div>
   )
 }
-
+// Mostrando fecha actual
 const MostrarFecha = () =>{
   const now = new Date()
   console.log(now)
@@ -15,7 +28,7 @@ const MostrarFecha = () =>{
     </div>
   )
 }
-
+// suma dos valores
 const Suma = () =>{
   const a = 10
   const b = 20
@@ -28,17 +41,145 @@ const Suma = () =>{
     </div>
   )
 }
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/jeanmunoz23'>jeanmunoz23</a>
+    </div>
+  )
+}
+
+//lista de amigos con fecha
+const Friends = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+// boton accionador
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
+// funcion que aumenta o incrementa o pone en cero el counter
+const Counter = () => {
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display value={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+const LeftOrRight = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+      <p>Total Clicks {total}</p>
+    </div>
+  )
+}
+const Thousand = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+    )
+}
+// app principal
 const App = () => {
+
   const name = 'Peter'
   const age = 10
   return (
     <div>
       <h1>Greetings</h1>
       {/* <Hello name='George' /> */}
+      <Counter/>
+      <LeftOrRight/>
+      <Thousand/>
       <Hello name='Daisy' age={26 + 10} />
       <Hello name={name} age={age} />
       <MostrarFecha/>
       <Suma/>
+      <Friends/>
+      <Footer/>
+
     </div>
   )
 }
