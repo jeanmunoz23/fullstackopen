2522bdcbcdc0460d6528856a6dd18af53045b351
jemprojects <jemprojectsgit@gmail.com>
2023-12-29 13:41:23 -0300

    completo ejercicio 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,26 +4,36 @@ const Number = ({ person }) => <div>{person.name}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
     if (persons.find((person) => person.name === newName))
       alert(`${newName} is already added to phonebook`)
     else {
-      const nameObject = {
-        name: newName
+      const personObject = {
+        name: newName,
+        number: newNumber
       }
-      setPersons(persons.concat(nameObject))
+      setPersons(persons.concat(personObject))
     }
     setNewName('')
+    setNewNumber('')
   }
 
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,6 +45,11 @@ const App = () => {
                   value={newName}
                   onChange={handleNameChange}
                 />
+        </div>
+        <div>number: <input
+                      value={newNumber}
+                      onChange={handleNumberChange}
+                    />
         </div>
         <div>
           <button type="submit">add</button>
